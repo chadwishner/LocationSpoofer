3c61a5a228ee7c403fe8612b08ac02b49fe214ec
Chad Wishner <chadwishner@gmail.com>
2025-03-13 17:44:01 -0400

    Initial Commit

---
+++ LocationSpoofer/LocationSpooferApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSpooferApp.swift
+//  LocationSpoofer
+//
+//  Created by Chad Wishner on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationSpooferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocationSpooferTests/LocationSpooferTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSpooferTests.swift
+//  LocationSpooferTests
+//
+//  Created by Chad Wishner on 3/13/25.
+//
+
+import Testing
+@testable import LocationSpoofer
+
+struct LocationSpooferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
